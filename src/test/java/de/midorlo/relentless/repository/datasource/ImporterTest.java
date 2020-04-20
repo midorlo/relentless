aa8034f3d2b87130b74cd1157a647734b0af8e16
midorlo <dorloechter@gmail.com>
2020-04-20 14:56:44 +0200

    readd repository tests

--- src/test/java/de/midorlo/relentless/repository/datasource/ImporterTest.java
+++ src/test/java/de/midorlo/relentless/repository/datasource/ImporterTest.java
@@ -1,32 +1,38 @@
 package de.midorlo.relentless.repository.datasource;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+
+import de.midorlo.relentless.domain.items.*;
+import de.midorlo.relentless.importer.*;
+import de.midorlo.relentless.repository.*;
+import org.testng.annotations.Test;
 
 public class ImporterTest {
 
-//    ObjectsRepository<Perk> perkRepository = new ObjectsRepository<>();
-//    ObjectsRepository<PerkEffect> perkEffectRepository = new ObjectsRepository<>();
-//    ObjectsRepository<Cell> cellRepository = new ObjectsRepository<>();
-//    ObjectsRepository<Weapon> weaponRepository = new ObjectsRepository<>();
-//    ObjectsRepository<Armor> armorRepository = new ObjectsRepository<>();
-//    ObjectsRepository<Lantern> lanternRepository = new ObjectsRepository<>();
-//
-//    @Test
-//    public void testImportGameObjects() {
-//        PerkImporter perkImporter = new PerkImporter(perkRepository, perkEffectRepository);
-//        perkImporter.importGameObjects(DIR_DAUNTLESS_BUILDER_PERKS);
-//        CellImporter cellImporter = new CellImporter(cellRepository, perkRepository, perkEffectRepository);
-//        cellImporter.importGameObjects(DIR_DAUNTLESS_BUILDER_CELLS);
-//        WeaponImporter weaponImporter = new WeaponImporter(weaponRepository, perkRepository, perkEffectRepository);
-//        weaponImporter.importGameObjects(DIR_DAUNTLESS_BUILDER_WEAPONS);
-//        ArmorImporter armorImporter = new ArmorImporter(armorRepository, perkRepository, perkEffectRepository);
-//        armorImporter.importGameObjects(DIR_DAUNTLESS_BUILDER_ARMOR);
-//        LanternImporter lanternImporter = new LanternImporter(lanternRepository, perkRepository, perkEffectRepository);
-//        lanternImporter.importGameObjects(DIR_DAUNTLESS_BUILDER_LANTERNS);
-//
-//        assertThat(perkRepository.findAll().size(), equalTo(56));
-//        assertThat(cellRepository.findAll().size(), equalTo(56));
-//        assertThat(weaponRepository.findAll().size(), equalTo(138));
-//        assertThat(armorRepository.findAll().size(), equalTo(91));
-//    }
+    Repository<PerkEffect> perkEffectRepository = new PerkEffectsRepository();
+    Repository<Perk> perkRepository = new PerkRepository(perkEffectRepository);
+    Repository<Cell> cellRepository = new CellRepository(perkRepository);
+    Repository<Weapon> weaponRepository = new WeaponRepository(perkRepository);
+    Repository<Armor> armorRepository = new ArmorRepository(perkRepository);
+    Repository<Lantern> lanternRepository = new LanternRepository(perkRepository);
+
+    @Test
+    public void testImportGameObjects() {
+        PerkImporter perkImporter = new PerkImporter(perkRepository, perkEffectRepository);
+        perkImporter.importGameObjects();
+        CellImporter cellImporter = new CellImporter(cellRepository, perkRepository, perkEffectRepository);
+        cellImporter.importGameObjects();
+        WeaponImporter weaponImporter = new WeaponImporter(weaponRepository, perkRepository, perkEffectRepository);
+        weaponImporter.importGameObjects();
+        ArmorImporter armorImporter = new ArmorImporter(armorRepository, perkRepository, perkEffectRepository);
+        armorImporter.importGameObjects();
+        LanternImporter lanternImporter = new LanternImporter(lanternRepository, perkRepository, perkEffectRepository);
+        lanternImporter.importGameObjects();
+
+        assertThat(perkRepository.findAll().size(), equalTo(56));
+        assertThat(cellRepository.findAll().size(), equalTo(56));
+        assertThat(weaponRepository.findAll().size(), equalTo(138));
+        assertThat(armorRepository.findAll().size(), equalTo(91));
+    }
 }
