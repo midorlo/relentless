ed3320102f0cd54fdaa1ebb3cf934d6504026639
midorlo <dorloechter@gmail.com>
2020-04-21 16:09:00 +0200

    (2) replace example with poc for class Hitzone.java

--- src/main/java/de/midorlo/relentless/AppJPARelentless.java
+++ src/main/java/de/midorlo/relentless/AppJPARelentless.java
@@ -2,20 +2,18 @@ package de.midorlo.relentless;
 
 import de.midorlo.relentless.domain.behemoth.Hitzone;
 import de.midorlo.relentless.repository.HitzoneRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.java.Log;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class AccessingDataJpaApplication {
-
-    private static final Logger log = LoggerFactory.getLogger(AccessingDataJpaApplication.class);
+@Log
+public class AppJPARelentless {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccessingDataJpaApplication.class);
+        SpringApplication.run(AppJPARelentless.class);
     }
 
     @Bean
@@ -23,9 +21,8 @@ public class AccessingDataJpaApplication {
         return args -> {
             repository.save(new Hitzone("H1"));
             repository.save(new Hitzone("H2"));
-
             Iterable<Hitzone> all = repository.findAll();
-            all.forEach(System.out::println);
+            all.forEach(hitzone -> log.info(hitzone.toString()));
         };
     }
 }
