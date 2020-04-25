b00064864446bca8566a97d37c04a59d9156403e
midorlo <dorloechter@gmail.com>
2020-04-25 22:01:59 +0200

    (2) indev restful for celltype, cell, perk

--- src/main/java/de/midorlo/relentless/domain/cell/CellType.java
+++ src/main/java/de/midorlo/relentless/domain/cell/CellType.java
@@ -1,22 +1,20 @@
 package de.midorlo.relentless.domain.cell;
 
-import de.midorlo.relentless.domain.NamedObject;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.io.Serializable;
 
 /**
  * The type of a Cell.
  * Currently of (Technique,Utility,Power,Mobility,Defence,Prismatic)
  */
-@EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
-public class CellType extends NamedObject {
+public class CellType {
 
+    @Id
+    private String name;
 
     public CellType() {}
 
