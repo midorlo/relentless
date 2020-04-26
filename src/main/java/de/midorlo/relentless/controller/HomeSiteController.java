b636224d4e49ce2652207af809ac569bdd5a0fd3
midorlo <dorloechter@gmail.com>
2020-04-26 16:16:23 +0200

    change minimal site example to demo usage

--- src/main/java/de/midorlo/relentless/controller/HomeSiteController.java
+++ src/main/java/de/midorlo/relentless/controller/HomeSiteController.java
@@ -6,12 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeSiteController {
 
-
-    @GetMapping("/")
-    public String greeting() {
-        return "index";
-    }
-
     @GetMapping("/config.js")
     public String fileConfig() {
         return "config.js";
